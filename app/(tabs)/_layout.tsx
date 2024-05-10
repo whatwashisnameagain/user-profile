cb7cdbbbdec8323d412856401a57777fdc949942
Reuben Remy <reubenremy@gmail.com>
2024-05-10 17:57:43 -0400

    add profile screen placeholders

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -20,8 +20,8 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />
+          title: 'Profile',
+          tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />
         }}
       />
       <Tabs.Screen
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,69 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 
 export default function ProfileScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Edit Profile</Text>
+      <View style={styles.profileImageContainer}>
+        <Image 
+          source={{uri: `https://randomuser.me/api/portraits/women/${Math.floor(Math.random() * 100)}.jpg`}}
+          style={styles.profileImage} // Adjust dimensions and styles as needed
+        />
+      </View>
+      <View style={styles.bioContainer}>
+        <Text>Bio</Text>
+      </View>
+      <View style={styles.socialContainer}>
+        <Text>Socials</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  bioContainer: {
+    borderWidth: 1,
+    borderColor: "red",
+    width: '100%',
+    height: 100,
+    marginBottom: '5%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   container: {
     flex: 1,
     alignItems: 'center',
+    padding: '20%'
+  },
+  socialContainer: {
+    borderWidth: 1,
+    borderColor: "blue",
+    width: '100%',
+    height: 100,
+
     justifyContent: 'center',
+    alignItems: 'center'
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
-  }
+    marginBottom: '10%'
+  },
+  profileImageContainer: {
+    height: 200,
+    width: 200,
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: "green",
+    borderRadius: 100,
+    justifyContent:'center',
+    alignItems: 'center',
+    marginBottom: "10%"
+  },
+  profileImage: {
+    height: 190,
+    width: 190,
+    borderRadius: 100
+  },  
 });
